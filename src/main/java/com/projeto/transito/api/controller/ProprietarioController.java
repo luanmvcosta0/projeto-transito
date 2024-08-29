ccd39c75d37dc1b77dc097e653c4fad7d1ea6ee9
luanmvcosta0 <luanmvcosta@mgail.com>
2024-08-28 21:03:15 -0300

    adicionando metodo findAll

--- src/main/java/com/projeto/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/projeto/transito/api/controller/ProprietarioController.java
@@ -1,8 +1,10 @@
 package com.projeto.transito.api.controller;
 
 import com.projeto.transito.domain.model.Proprietario;
+import com.projeto.transito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,17 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
+    @Autowired
+    private ProprietarioRepository proprietarioRepository;
+
     @GetMapping("/teste")
     public String teste() {
         return "Teste concluído!";
+    }
+
+    @GetMapping("/proprietarios")
+    public List<Proprietario> listar() {
+        return proprietarioRepository.findAll();
     }
 
 }
